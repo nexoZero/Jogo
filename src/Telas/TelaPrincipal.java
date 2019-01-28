db72cb483863d0b5eb179844885ea32449144973
nexoZero <sigma.leonnard@gmail.com>
2019-01-28 13:52:27 -0300

    alteracao 3

--- src/Telas/TelaPrincipal.java
+++ src/Telas/TelaPrincipal.java
@@ -3,6 +3,8 @@ package Telas;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -13,17 +15,28 @@ public class TelaPrincipal extends JFrame{
     private JPanel painel;
     private JButton botao1;   
     private JButton botao2;
-    private ControleBotoesSelecionados controle ;    
+    private JButton botao3;
+    private JButton botao4;
+    private ControleBotoesSelecionados controle1 ;
+    private ControleBotoesSelecionados controle2 ;    
+    private List<ControleBotoesSelecionados> listControle;
     
     public TelaPrincipal(){
         super("Jogo da Memoria"); //Nome da Janela
-        controle = new ControleBotoesSelecionados();
-        controle.setNmBotoes("hentai");
+
+        listControle = new ArrayList<>();
         
         ActionListener acaoBotao = new  ActionListener() {
             @Override
             public void actionPerformed(ActionEvent ae) {
-                 controle.executarAcaoBotao(((JButton) ae.getSource()),StatusBotao.SELECIONADO);
+                JButton botao = (JButton) ae.getSource();
+                
+                    for(ControleBotoesSelecionados controleTemp : listControle){
+                        if(controleTemp.getRefBotao().get(botao) != null){
+                            controleTemp.executarAcaoBotao(((JButton) ae.getSource()),StatusBotao.SELECIONADO);
+                        }
+                    }
+                
                  // ((JButton) ae.getSource()) e uma forma generica de dizer que o botao foi precionado
             }
             
@@ -33,6 +46,11 @@ public class TelaPrincipal extends JFrame{
         this.painel = new JPanel();
         this.add(painel); //esse metodo adiciona o painel na janela
         painel.setLayout(null); // esse metodo gerencia os componentes dentro do painel ao colocar null a posicao dos componetes depende do metodo setBounds
+                
+        controle1 = new ControleBotoesSelecionados();
+        controle1.setNmBotoes("hentai");
+        controle2 = new ControleBotoesSelecionados();
+        controle2.setNmBotoes("Orgia");
         
         botao1 = new JButton("jogo");
         painel.add(botao1);
@@ -66,9 +84,24 @@ public class TelaPrincipal extends JFrame{
         botao2.setBounds(120,10,100,100);//definir tamanho e posicao do botao no painel
         botao2.addActionListener(acaoBotao); //adicionar uma acao ao botao
         
-        this.controle.adicionarBotao(botao1);
-        this.controle.adicionarBotao(botao2);
+        botao3 = new JButton("jogo");
+        painel.add(botao3); //adicionar o botao no painel
+        botao3.setBounds(10, 120, 100, 100); //definir tamanho e posicao do botao no painel
+        botao3.addActionListener(acaoBotao); //adiciona uma ação ao botao
+        
+        botao4 = new JButton("jogo"); //instancinado o botao
+        painel.add(botao4); //adicionar o botao no painel
+        botao4.setBounds(120, 120, 100, 100); // definir tamanho e posicao no painel
+        botao4.addActionListener(acaoBotao); //adiciona uma ação ao botao
+        
+        this.controle1.adicionarBotao(botao1);
+        this.controle1.adicionarBotao(botao2);
+        
+        this.controle2.adicionarBotao(botao3);
+        this.controle2.adicionarBotao(botao4);
 
+        this.listControle.add(controle1);
+        this.listControle.add(controle2);
         
         this.setBounds(500, 150, 350, 350);//tamanho e posição da tela(x, y, largura, altura )
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // fechar a Janela, encerar a aplicao quando clicado
